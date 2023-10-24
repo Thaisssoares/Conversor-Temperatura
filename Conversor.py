633e263eabf8e1a2da6b9945481858861bd5b9a8
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-10-24 13:24:45 -0300

    -Commit Thais conversor -Commit Thais

--- Conversor.py
+++ Conversor.py
@@ -1,11 +1,6 @@
-#Conversor de Temperatura
-#Processamento e Saída
-def multiplicacao (n1 , n2,n3,n4):
-    return n1 * n2/ n3 + n4
 
-def sub (s1,s2,s3,s4):
-    return (s1 - s2)/s3 * s4
+def fah (cel):
+    return (9 * cel /5+32)
 
-#Teste
-print('f {} * {}/ {} + {}' {multiplicacao( 9,32,5,32 )})
-print('f {} * {}/ {} + {}' {sub( 38,32,9,5 )})
+def cel (fah):
+    return (fah-32)/9 * 5
--- Temperatura.py
+++ Temperatura.py
@@ -1,39 +1,35 @@
-from Conversor import multiplicacao
-from Conversor import sub
+from Conversor import fah,cel
 
 while True:
     #Apresentação
-    print('\n\n\n\t ---Conversor de temperatura--\n')
+    print('\n\n\n\t ---Conversor de temperatura Fah e Cel--\n')
     #Menu
-    print('1.multiplicacao')
-    print('2.subtraçao')
+    print('1.Fahrenheit')
+    print('2.Celsius')
     print('3.Sair')
     #Ler a opção de escolha do usuário
     op=int(input('\n opção:'))
     if op == 1 :
-        print('\nSoma\n')
+        print('\n Fahrenheit \n')
         #Entrada
         n1=int(input('digite um numero:'))
-        n2=int(input('digite segundo numero:'))
-        n3=int(input('digite terceiro numero:'))
-        n4=int(input('digite quarto valor:'))
+
         #Processamento
-        total=multiplicacao(n1,n2,n3,n4)
+        total=fah(n1)
         #Saída
         print(f'\n {n1}*{n2}/{n3}+{n4} = {total}\n')
     elif op == 2 :
-        print('\nSubtração\n')
+        print('\n Celsius \n')
         #Entrada
         s1=int(input('escreva um numero:'))
-        s2=int(input('escreva segundo numero:'))
-        s3=int(input('escreva terceiro numero:'))
-        s4=int(input('escreva quarto numero'))
         #Processamento
-        total2= sub(s1,s2,s3,s4)
+        total2= cel(s1)
         #Saída
         print('f\n ({s1} - {s2)\ {s3} * {s4}')
     elif op == 3:
         #Sair do sistema
-        print('Bom trabalho,sair do projeto')
+        print('Bom trabalho!')
+    break
+
     else:
         print(f'\n opção {op} incorreta\n')
